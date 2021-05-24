83bb6c9243bd8c16e301ab423f974f8cfe4f3c20
delphe <delphedwin@gmail.com>
2021-05-24 01:26:58 +0000

    Deploying to gh-pages from master 8a77bb3ea0038487da2e14ec8aeb9ac7602014bb

---
+++ precache-manifest.67293b883180bfa95570687bbff5362b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f90c99581e9af7052242bc562b6a61c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "15eb3e51c8e3650cacd7",
+    "url": "./static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "93772a4726c45952dc26",
+    "url": "./static/css/main.b55abf3b.chunk.css"
+  },
+  {
+    "revision": "15eb3e51c8e3650cacd7",
+    "url": "./static/js/2.04c65762.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.04c65762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93772a4726c45952dc26",
+    "url": "./static/js/main.2cfbabcf.chunk.js"
+  },
+  {
+    "revision": "a37c8e9ce5cfd2e91b17",
+    "url": "./static/js/runtime-main.d3467417.js"
+  },
+  {
+    "revision": "9fe335702196900941e88c2add69c7ef",
+    "url": "./static/media/logo.9fe33570.svg"
+  }
+]);
